84caf0b728b20157f5aa47c9e7e6184575e8a60e
jdesgarennes <johndesgarennes@gmail.com>
2021-10-23 23:06:35 -0700

    MVP created

---
+++ index.js
@@ -0,0 +1,157 @@
+
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const myTeam = require("./src/pageTemplate");
+
+
+// all of the modules created in lib folder
+
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const Deadweight = require("./lib/deadweight");
+
+// My array for all the collected data of the team individuals
+const newTeamMembersData = [];
+
+// Array object questions asked in CLI to user
+const questions = async () => {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the new team members name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the team member ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is there current email address?",
+        name: "email",
+      },
+      {
+        type: "list",
+
+        message: "What would you say you do around here?",
+
+        name: "role",
+
+        choices: ["Engineer", "Intern", "Manager","Deadweight"],
+
+      },
+    ])
+
+
+
+      if (answers.role === "Manager") {
+        const managerAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your current office number?",
+              name: "officeNumber",
+            },
+          ])
+
+          const newManager = new Manager(
+
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+          );
+          newTeamMembersData.push(newManager);
+          
+        // Enginner specific questions
+      } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your GitHub user name?",
+              name: "github",
+            }
+          ])
+            const newEngineer = new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              githubAns.github
+            );
+            newTeamMembersData.push(newEngineer);
+          
+        // Intern specific role questions
+      } else if (answers.role === "Intern") {
+        const internAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What university did you attend?",
+              name: "school",
+            },
+          ])
+          
+          const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school
+          );
+          newTeamMembersData.push(newIntern); 
+
+          // DeadWeight specific role question         
+      } else if (answers.role === "Deadweight") {
+        const deadweightAns = await inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your facebook profile that you use at work all day?",
+              name: "facebook",
+            },
+          ])
+          
+          const newDeadWeight = new Deadweight(
+            answers.name,
+            answers.id,
+            answers.email,
+            deadweightAns.facebook
+          );
+          newTeamMembersData.push(newDeadWeight);          
+      } 
+}; 
+
+async function promptQuestions() {
+  await questions()
+    
+  
+  const addMemberAns = await inquirer
+    .prompt([
+      {
+        name:'addMember',
+        type: 'list',
+        choices: ['Add a new member', 'Create team'],
+        message: "What would you like to do next?"
+      }
+    ])
+
+    if (addMemberAns.addMember === 'Add a new member') {
+      return promptQuestions()
+    }
+    return createTeam();
+}  
+
+promptQuestions();
+
+function createTeam () {
+  console.log("new guy", newTeamMembersData)
+  fs.writeFileSync(
+    "./dist/index.html",
+    myTeam(newTeamMembersData),
+    "utf-8"
+  );
+}
---
+++ src/pageTemplate.js
@@ -0,0 +1,144 @@
+const myTeam = (team) => {
+    // create the manager html
+
+
+    const genManager = (manager) => {
+      return `
+          <div class="card employee-card">
+          <div class="card-header">
+              <h2 class="card-title">${manager.getName()}</h2>
+              <h3 class="card-title"><i class="far fa-angry mr-2"></i>${manager.getTitle()}</h3>
+          </div>
+          <div class="card-body">
+              <ul class="list-group">
+                  <li class="list-group-item">ID: ${manager.getId()}</li>
+                  <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                  <li class="list-group-item">Office number: ${manager.getOffice()}</li>
+              </ul>
+          </div>
+      </div>
+          `;
+    };
+  
+    // create the html for engineers
+    const genEngineer = (engineer) => {
+      return `
+          <div class="card employee-card">
+      <div class="card-header">
+          <h2 class="card-title">${engineer.getName()}</h2>
+          <h3 class="card-title"><i class="fas fa-ruler-combined mr-2"></i>${engineer.getTitle()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${engineer.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+              <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+          </ul>
+      </div>
+  </div>
+          `;
+    };
+  
+    // create the html for interns
+    const genIntern = (intern) => {
+      return `
+          <div class="card employee-card">
+      <div class="card-header">
+          <h2 class="card-title">${intern.getName()}</h2>
+          <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getTitle()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${intern.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+              <li class="list-group-item">School: ${intern.getSchool()}</li>
+          </ul>
+      </div>
+  </div>
+          `;
+    };
+  
+// creates the html for deadWeight
+const gendeadweight = (deadweight) => {
+    return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${deadweight.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-weight-hanging mr-2"></i>${deadweight.getTitle()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${deadweight.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${deadweight.getEmail()}">${deadweight.getEmail()}</a></li>
+            <li class="list-group-item">Facebook: <a href="https://facebook.com/public/${deadweight.getFacebook()}" target="_blank" rel="noopener noreferrer">${deadweight.getFacebook()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
+
+
+
+
+    const html = [];
+  
+    html.push(
+      team
+        .filter((employee) => employee.getTitle() === "Manager")
+        .map((manager) => genManager(manager))
+    );
+    html.push(
+      team
+        .filter((employee) => employee.getTitle() === "Engineer")
+        .map((engineer) => genEngineer(engineer))
+        .join("")
+    );
+    html.push(
+      team
+        .filter((employee) => employee.getTitle() === "Intern")
+        .map((intern) => genIntern(intern))
+        .join("")
+    );
+    html.push(
+        team
+          .filter((employee) => employee.getTitle() === "Deadweight")
+          .map((deadweight) => gendeadweight(deadweight))
+          .join("")
+      );
+    return html.join("");
+  };
+  
+  // export function to generate entire page
+  module.exports = (team) => {
+    return `
+      <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-heading">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="team-area col-12 d-flex justify-content-center">
+                  ${myTeam(team)}
+              </div>
+          </div>
+      </div>
+  </body>
+  </html>
+      `;
+  };
